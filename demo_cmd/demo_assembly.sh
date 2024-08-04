c61fdaf46b81e6564a648e4ea67871f7fb4c086a
yuh2k <yuhang20518@gmail.com>
2024-08-03 21:32:57 -0700

    update assembly and alignment

--- demo_cmd/demo_assembly.sh
+++ demo_cmd/demo_assembly.sh
@@ -1,1 +1,2 @@
-./bin/metahit-modules/assembly.sh -1 test_data/sim_sg_R1.fastq -2 test_data/sim_sg_R2.fastq -o output_dir -m 24 -t 4 --megahit
+# Copy below command to the root directory
+./bin/metahit-modules/assembly.sh -1 test_data/sim_sg_R1_test.fastq -2 test_data/sim_sg_R2_test.fastq -o output_dir -m 24 -t 4 --megahit
