3a51d9de70f666c420645c86d4c3c7ea08401dfb
yuh2k <yuhang20518@gmail.com>
2024-08-18 01:40:18 -0700

    alignment refinement

--- bin/metahit-modules/alignment.sh
+++ bin/metahit-modules/alignment.sh
@@ -6,25 +6,26 @@
 # Aligns reads to the final assembly and processes the output using SamTools.
 #############################################################################################################
 
-# Usage
-echo "Usage: metahit alignment -f final_assembly.fasta -1 HIC_1_dedup.fastq.gz -2 HIC_2_dedup.fastq.gz -o output_dir"
+# Display usage if not enough arguments
+if [ "$#" -ne 8 ]; then
+    echo "Usage: $0 -f final_assembly.fasta -1 HIC_1_dedup.fastq.gz -2 HIC_2_dedup.fastq.gz -o output_dir"
+    exit 1
+fi
 
-# Parameters
-fasta="final_assembly.fasta"
-reads_1="HIC_1_dedup.fastq.gz"
-reads_2="HIC_2_dedup.fastq.gz"阿
-output_dir=".output/final/"
+# Read in the arguments
+while getopts f:1:2:o: flag
+do
+    case "${flag}" in
+        f) fasta=${OPTARG};;
+        1) reads_1=${OPTARG};;
+        2) reads_2=${OPTARG};;
+        o) output_dir=${OPTARG};;
+    esac
+done
 
-# Run BWA MEM to align reads
-bwa mem -5SP $fasta $reads_1 $reads_2 > $output_dir/MAP.sam
-
-# Convert SAM to BAM, filtering out unwanted flags
-samtools view -F 0x904 -bS $output_dir/MAP.sam > $output_dir/MAP_UNSORTED.bam
-
-# Sort BAM file by read names
-samtools sort -n $output_dir/MAP_UNSORTED.bam -o $output_dir/MAP_SORTED.bam
-
-# Clean up
-rm $output_dir/MAP.sam $output_dir/MAP_UNSORTED.bam
+mkdir -p "$output_dir"
+bwa mem -5SP "$fasta" "$reads_1" "$reads_2" > "$output_dir/MAP.sam"
+samtools view -F 0x904 -bS "$output_dir/MAP.sam" > "$output_dir/MAP_UNSORTED.bam"
+samtools sort -n "$output_dir/MAP_UNSORTED.bam" -o "$output_dir/MAP_SORTED.bam"
 
 echo "Alignment completed successfully!"
